27ae37686a1637b9c9b9e7361a8e1d7b4ffcf69d
siddharth171 <sidhrth05@gmail.com>
2020-10-15 23:09:48 +0530

    share post and comment

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 # Create your views here.
@@ -42,9 +42,28 @@ def post_detail(request, year, month, day, post):
                                    publish__year=year,
                                    publish__month=month,
                                    publish__day=day)
+    # list of active comments fot this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            #Create comment but don't save to the comment
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})  
+                  {'post': post,
+                   'comments':comments,
+                   'new_comment':new_comment,
+                   'comment_form':comment_form})  
 
 
 def post_share(request, post_id):
