57e50718b42ccfafe6e86ef1f9b127077e36efc1
siddharth171 <sidhrth05@gmail.com>
2020-10-30 20:03:51 +0530

    custom template and css changes

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,9 +8,10 @@ from django.core.mail import send_mail
 
 from taggit.models import Tag
 
+from django.db.models import Count
 # Create your views here.
-'''
-def post_list(request):
+
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
@@ -23,26 +24,33 @@ def post_list(request):
         # If page is out of range, deliver last page of results
         posts = paginator.page(paginator.num_pages)
 
-    return render(request,
-                  'blog/post/list.html',
-                  {'page':page,'posts':posts})
-'''
-
-def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     tag = None
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
-        object_list = object_list.filter(tags__in=[tag]) 
+        object_list = object_list.filter(tags__in=[tag])
 
-    return render(request, 'blog/post/list.html', {'tag':tag})
+    return render(request,
+                  'blog/post/list.html',
+                  {'page':page,'posts':posts,
+                  'tag':tag})
+
+
+# def post_list(request, tag_slug=None):
+#     object_list = Post.published.all()
+#     tag = None
+#     if tag_slug:
+#         tag = get_object_or_404(Tag, slug=tag_slug)
+#         object_list = object_list.filter(tags__in=[tag]) 
+
+#     return render(request, 'blog/post/list.html', {'tag':tag})
 
 
 
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 10
+    paginate_by = 4
     template_name = 'blog/post/list.html'
 
 
@@ -72,12 +80,20 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags','-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments':comments,
                    'new_comment':new_comment,
-                   'comment_form':comment_form})  
+                   'comment_form':comment_form,
+                   'similar_posts':similar_posts})  
 
 
 def post_share(request, post_id):
