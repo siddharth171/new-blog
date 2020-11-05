f5cf618c176efeb1865335aa5faad343c682e487
siddharth171 <sidhrth05@gmail.com>
2020-11-05 17:08:41 +0530

    creating text-search engine

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,12 +3,14 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 
 from taggit.models import Tag
 
 from django.db.models import Count
+
+from django.contrib.postgres.search import SearchVector
 # Create your views here.
 
 def post_list(request, tag_slug=None):
@@ -119,3 +121,21 @@ def post_share(request, post_id):
                                                     'form':form,
                                                     'sent':sent})
 
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if query in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search = SearchVector('title','body'),
+            ).filter(search=query)
+    
+    return render(request, 'blog/post/search.html',
+                 {'form':form,
+                  'query':query,
+                  'results':results})
